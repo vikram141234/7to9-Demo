e4340ed074ae00fa61012558ec39525b73d6fa1a
vikra <vikra@Vicky>
2023-04-28 14:07:16 +0530

    commit and push

---
+++ src/test/java/Practice/Demo3.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		int a=11,b=20,c=30,d=40,e;
+		e=d%a;
+		System.out.println(a+e+"\n"+"Third Commit");
+
+	}
+
+}
